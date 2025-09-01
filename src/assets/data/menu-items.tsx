f3e5721de052598c05775d4ac6d833f8ad709feb
maaaribsalar-ux <maaaribsalar@gmail.com>
2025-09-01 18:32:16 +0500

    Update menu-items.tsx

--- src/assets/data/menu-items.tsx
+++ src/assets/data/menu-items.tsx
@@ -25,137 +25,60 @@ const MENU_ITEMS: MenuItemTypes[] = [
     isTitle: false,
   },
   {
+    key: 'save-tiktok-video',
+    label: 'Home',
+    url: '/blogs/save-tiktok-video',
+    isTitle: false,
+  },
+  {
+    key: 'tiktok-story-downloader',
+    label: 'Tiktok Story Downloader',
+    url: '/tiktok-story-downloader',
+    isTitle: false,
+  },
+  {
     key: 'tiktok-downloader',
     label: 'Tiktok Downloader',
     isTitle: false,
     children: [
       {
-        key: 'tiktok-story-downloader',
-        label: 'Tiktok Story Downloader',
-        url: '/tiktok-downloader/tiktok-story-downloader',
+        key: 'tiktok-video-downloader',
+        label: 'Tiktok Video Downloader',
+        url: '/tiktok-video-downloader',
         parentKey: 'tiktok-downloader',
       },
+      {
+        key: 'download-tiktok-mp3',
+        label: 'Download TikTok MP3',
+        url: '/download-tiktok-mp3',
+        parentKey: 'tiktok-downloader',
+      },
+      {
+        key: 'download-tiktok-mp3',
+        label: 'Download TikTok MP3',
+        url: '/download-tiktok-mp3',
+        parentKey: 'tiktok-downloader',
+      },
+      {
+        key: 'tiktok-photo-downloader',
+        label: 'TikTok Photo Downloader',
+        url: '/tiktok-photo-downloader',
+        parentKey: 'tiktok-downloader',
+      },
+
+      
     ],
   },
-  {
-    key: 'pages',
-    label: 'Pages',
-    isTitle: false,
-    children: [
-      {
-        key: 'company',
-        label: 'Company',
-        url: '/pages/company',
-        parentKey: 'pages',
-      },
-      {
-        key: 'dashboard',
-        label: 'Dashboard',
-        url: '/dashboard',
-        parentKey: 'pages',
-      },
-      {
-        key: 'career',
-        label: 'Career',
-        url: '/pages/career',
-        parentKey: 'pages',
-      },
-      {
-        key: 'pricing',
-        label: 'Pricing',
-        url: '/pages/pricing',
-        parentKey: 'pages',
-      },
-      {
-        key: 'setting',
-        label: 'Setting',
-        url: '/setting',
-        parentKey: 'pages',
-      },
-      {
-        key: 'help',
-        label: 'Help',
-        url: '/pages/help',
-        parentKey: 'pages',
-      },
-    ],
-  },
-  {
-    key: 'portfolio',
-    label: 'Portfolio',
-    isTitle: false,
-    children: [
-      {
-        key: 'portfolio-grid',
-        url: '/pages/portfolio/grid',
-        label: 'Portfolio Grid',
-        parentKey: 'portfolio',
-      },
-      {
-        key: 'portfolio-masonry',
-        url: '/pages/portfolio/masonry',
-        label: 'Portfolio Masonry',
-        parentKey: 'portfolio',
-      },
-      {
-        key: 'portfolio-item',
-        url: '/pages/portfolio/item',
-        label: 'Portfolio Item',
-        parentKey: 'portfolio',
-      },
-    ],
-  },
+   
   {
     key: 'blogs',
     label: 'Blog',
-    parentKey: 'Pages',
+    url: '/pages/blogs/list',
+    parentKey: 'blogs',
     isDivider: false,
-    children: [
-      {
-        key: 'blog-page',
-        label: 'Blog',
-        url: '/pages/blogs/list',
-        parentKey: 'blogs',
-      },
-      {
-        key: 'blog-post',
-        label: 'Blog Post',
-        url: '/pages/blogs/post',
-        parentKey: 'blogs',
-      },
-    ],
+    
   },
-  {
-    key: 'auth',
-    label: 'Account',
-    isTitle: false,
-    children: [
-      {
-        key: 'auth-login',
-        label: 'Login',
-        url: '/auth/login',
-        parentKey: 'auth',
-      },
-      {
-        key: 'auth-register',
-        label: 'Sign Up',
-        url: '/auth/register',
-        parentKey: 'auth',
-      },
-      {
-        key: 'auth-forgot-password',
-        label: 'Forget Password',
-        url: '/auth/forgot-password',
-        parentKey: 'auth',
-      },
-      {
-        key: 'auth-confirm-account',
-        label: 'Confirm Account',
-        url: '/auth/confirm-account',
-        parentKey: 'auth',
-      },
-    ],
-  },
+  
   {
     key: 'contact-us',
     label: 'Contact us',
@@ -168,184 +91,69 @@ const HORIZONTAL_MENU_ITEMS: MenuItemTypes[] = [
   {
     key: 'home',
     label: 'Home',
-    url: '/',
-    isTitle: true,
+    url: '/home',
+    isTitle: false,
+  },
+  {
+    key: 'save-tiktok-video',
+    label: 'Home',
+    url: '/blogs/save-tiktok-video',
+    isTitle: false,
+  },
+  {
+    key: 'tiktok-story-downloader',
+    label: 'Tiktok Story Downloader',
+    url: '/tiktok-story-downloader',
+    isTitle: false,
   },
   {
     key: 'tiktok-downloader',
     label: 'Tiktok Downloader',
-    isTitle: true,
+    isTitle: false,
     children: [
       {
-        key: 'tiktok-story-downloader',
-        label: 'Tiktok Story Downloader',
-        url: '/tiktok-downloader/tiktok-story-downloader',
+        key: 'tiktok-video-downloader',
+        label: 'Tiktok Video Downloader',
+        url: '/tiktok-video-downloader',
         parentKey: 'tiktok-downloader',
       },
+      {
+        key: 'download-tiktok-mp3',
+        label: 'Download TikTok MP3',
+        url: '/download-tiktok-mp3',
+        parentKey: 'tiktok-downloader',
+      },
+      {
+        key: 'download-tiktok-mp3',
+        label: 'Download TikTok MP3',
+        url: '/download-tiktok-mp3',
+        parentKey: 'tiktok-downloader',
+      },
+      {
+        key: 'tiktok-photo-downloader',
+        label: 'TikTok Photo Downloader',
+        url: '/tiktok-photo-downloader',
+        parentKey: 'tiktok-downloader',
+      },
+
+      
     ],
   },
+   
   {
-    key: 'pages',
-    label: 'Pages',
-    isTitle: true,
-    children: [
-      {
-        key: 'pages-auth',
-        label: 'Account',
-        parentKey: 'pages',
-        children: [
-          {
-            key: 'auth-login',
-            label: 'Login',
-            url: '/auth/login',
-            parentKey: 'pages-auth',
-          },
-          {
-            key: 'auth-register',
-            label: 'Sign Up',
-            url: '/auth/register',
-            parentKey: 'pages-auth',
-          },
-          {
-            key: 'auth-forgot-password',
-            label: 'Forget Password',
-            url: '/auth/forgot-pass',
-            parentKey: 'pages-auth',
-          },
-          {
-            key: 'auth-confirm-account',
-            label: 'Confirm Account',
-            url: '/auth/confirm-account',
-            parentKey: 'pages-auth',
-          },
-        ],
-      },
-      {
-        key: 'pages-blogs',
-        label: 'Blog',
-        parentKey: 'pages',
-        isDivider: true,
-        children: [
-          {
-            key: 'blog',
-            label: 'Blog',
-            url: '/pages/blogs/list',
-            parentKey: 'pages-blogs',
-          },
-          {
-            key: 'blog-post',
-            label: 'Blog Post',
-            url: '/pages/blogs/post',
-            parentKey: 'pages-blogs',
-          },
-        ],
-      },
-      {
-        key: 'pages-company',
-        label: 'Company',
-        url: '/pages/company',
-        parentKey: 'pages',
-      },
-      {
-        key: 'contact-us',
-        label: 'Contact',
-        url: '/pages/contact',
-        parentKey: 'pages',
-      },
-      {
-        key: 'pages-dashboard',
-        label: 'Dashboard',
-        url: '/dashboard',
-        parentKey: 'pages',
-      },
-      {
-        key: 'setting',
-        label: 'Setting',
-        url: '/setting',
-        parentKey: 'pages',
-      },
-      {
-        key: 'pages-career',
-        label: 'Career',
-        url: '/pages/career',
-        parentKey: 'pages',
-      },
-      {
-        key: 'pages-pricing',
-        label: 'Pricing',
-        url: '/pages/pricing',
-        parentKey: 'pages',
-      },
-      {
-        key: 'pages-portfolio',
-        label: 'Portfolio',
-        parentKey: 'pages',
-        isDivider: true,
-        children: [
-          {
-            key: 'portfolio-grid',
-            url: '/pages/portfolio/grid',
-            label: 'Portfolio Grid',
-            parentKey: 'pages-portfolio',
-          },
-          {
-            key: 'portfolio-masonry',
-            url: '/pages/portfolio/masonry',
-            label: 'Portfolio Masonry',
-            parentKey: 'pages-portfolio',
-          },
-          {
-            key: 'portfolio-item',
-            url: '/pages/portfolio/item',
-            label: 'Portfolio Item',
-            parentKey: 'pages-portfolio',
-          },
-        ],
-      },
-      {
-        key: 'pages-help',
-        label: 'Help',
-        url: '/pages/help',
-        parentKey: 'pages',
-      },
-    ],
+    key: 'blogs',
+    label: 'Blog',
+    url: '/pages/blogs/list',
+    parentKey: 'blogs',
+    isDivider: false,
+    
   },
+  
   {
-    key: 'docs',
-    label: 'Docs',
-    isTitle: true,
-    children: [
-      {
-        key: 'colors',
-        label: 'Colors',
-        url: '/docs/colors',
-        parentKey: 'docs',
-      },
-      {
-        key: 'typography',
-        label: 'Typography',
-        url: '/docs/typography',
-        parentKey: 'docs',
-      },
-      {
-        key: 'components',
-        label: 'Components',
-        url: '/docs/ui',
-        parentKey: 'docs',
-      },
-      {
-        key: 'custom',
-        label: 'Custom',
-        url: '/docs/custom',
-        parentKey: 'docs',
-      },
-      {
-        key: 'plugin',
-        label: 'Plugin',
-        url: '/docs/plugins',
-        parentKey: 'docs',
-      },
-    ],
+    key: 'contact-us',
+    label: 'Contact us',
+    url: '/contact-us',
+    isTitle: false,
   },
 ]
 
