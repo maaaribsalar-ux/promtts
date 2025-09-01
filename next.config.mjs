b6764f30ebe7c7af1f9da561e2a0610459804f87
maaaribsalar-ux <maaaribsalar@gmail.com>
2025-09-01 19:04:40 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+import nextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = nextIntlPlugin();
+
 const nextConfig = {
-  reactStrictMode:true,
+  reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'es'], // Supported locales
+    defaultLocale: 'en', // Default locale
+    localeDetection: true, // Automatically detect user's locale
+  },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
